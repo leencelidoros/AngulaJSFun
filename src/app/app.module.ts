c7d6dc2acf5bd8fc917f1a315c46f2066678096a
leence00 <leencelidonde00@gmail.com>
2023-08-25 13:07:45 +0300

    Getting Started with Angular Js

---
+++ src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { productListComponent } from './products/product-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    productListComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
